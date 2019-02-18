b638a9d562b78396e439008763013dab683f6fe4
Sergey Guchok <gck.sergey@gmail.com>
2019-02-18 21:52:45 +0300

    feat: implement money-exchange function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if (currency > 10000) {
+        return { error: "You are rich, my friend! We don't have so much coins for exchange" }
+    }
+    
+    if (currency < 0) {
+        return {};
+    }
+    
+    let result = {};
+
+    const currencyValue = {
+        H: 50,
+        Q: 25,
+        D: 10,
+        N: 5,
+        P: 1,
+    };
+
+    while(currency !== 0) {
+        for (let key in currencyValue) {
+            let amount = 0;
+            while(currency >= currencyValue[key]) {
+                currency -= currencyValue[key];
+                amount += 1;
+            }
+            if (amount !== 0) {
+                result[key] = amount;
+            }
+        }
+    }
+    return result;
 }
